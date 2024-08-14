2172278982fb867d0912a5f5f06019c99c2d43ed
Lembaad-imad <lembaad.imad.lm@gmail.com>
2024-08-14 09:39:42 +0100

    fix icon search v2

--- src/Home/Products/BottomNav.jsx
+++ src/Home/Products/BottomNav.jsx
@@ -7,7 +7,7 @@ const BottomNav = () => {
   const [activeIcon, setActiveIcon] = useState('home');
 
   return (
-    <div className="fixed navigation bottom-0  left-0 right-0 p-4 shadow-md">
+    <div className="fixed navigation bottom-0 z-100 left-0 right-0 p-4 shadow-md">
       <div className="menu w-full h-24 flex justify-center items-center">
         <div className='bill bg-red-500'>
           <i></i>
@@ -15,14 +15,14 @@ const BottomNav = () => {
         </div>
       </div>
 
-      <div className="fixed bottom-3 left-0 right-0 flex justify-around items-center">
+      <div className="fixed bottom-3 z-10 left-0 right-0 flex justify-around items-center">
         <div className="flex flex-col items-center" onClick={() => setActiveIcon('home')}>
           <FontAwesomeIcon icon={faHouse} className={`text-2xl  ${activeIcon === 'home' ? 'text-red-500' : 'text-gray-400'}`} />
           <span className={`text-xs ${activeIcon === 'home' ? 'text-red-500' : 'text-gray-400'}`}>Home</span>
         </div>
-        <div className="flex flex-col z-10  " onClick={() => setActiveIcon('search')}>
-          <FontAwesomeIcon icon={faMagnifyingGlass} className={`text-2xl mr-5 z-10 ${activeIcon === 'search' ? 'text-red-500' : 'text-gray-400'}`} />
-          <span className={`text-xs z-10 ${activeIcon === 'search' ? 'text-red-500' : 'text-gray-400'}`}>Search</span>
+        <div className="flex flex-col   " onClick={() => setActiveIcon('search')}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} className={`text-2xl search mr-5  ${activeIcon === 'search' ? 'text-red-500' : 'text-gray-400'}`} />
+          <span className={`text-xs  ${activeIcon === 'search' ? 'text-red-500' : 'text-gray-400'}`}>Search</span>
         </div>
         <div className="flex flex-col items-center" onClick={() => setActiveIcon('cart')}>
           <FontAwesomeIcon icon={faBagShopping} className={`text-2xl ${activeIcon === 'cart' ? 'text-red-500' : 'text-gray-400'}`} />
